fb9dde28eec9baadfbe14bf2047e7c93a5d004c0
ngohung1108 <ngohung11081996@gmail.com>
2019-10-23 10:43:36 +0700

    Updates

---
+++ precache-manifest.a32d6b747df117826f7adc38b995e5d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d1b8b6d165ad568bd79729ab74d219",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "0443d20593f032a20b98",
+    "url": "/reactApp/static/css/main.4f6e1d0a.chunk.css"
+  },
+  {
+    "revision": "5be570a41867c84f01e6",
+    "url": "/reactApp/static/js/2.d4a63849.chunk.js"
+  },
+  {
+    "revision": "0443d20593f032a20b98",
+    "url": "/reactApp/static/js/main.cac132eb.chunk.js"
+  },
+  {
+    "revision": "a949498934f0ed67143a",
+    "url": "/reactApp/static/js/runtime-main.c550209d.js"
+  }
+]);
